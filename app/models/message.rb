b8ec29e09112a4b293ff6f3606c064d55129ea3d
nippi-29 <pipimelody.1726@gmail.com>
2020-08-31 11:00:47 +0900

    画像のみでもメッセージ送信可能となるバリデーションの設定

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,5 +3,9 @@ class Message < ApplicationRecord
   belongs_to :room
   has_one_attached :image
 
-  validates :content, presence: true
+  validates :content, presence: true, unless: :was_attachd?
+
+  def was_attachd?
+    self.image.attached?
+  end
 end
